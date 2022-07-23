34401e55fc4ca4d4a2649d4f0d2a9f51d99b8c0e
Nazmul Haque <bpinazmul18@gmail.com>
2022-07-23 07:56:35 +0600

    Add new route.

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const home = require("../routes/home");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const inbox = require("../routes/inbox");
 const error = require("../middleware/error");
 const cookieParser = require("cookie-parser");
 const config = require("config");
@@ -13,13 +14,13 @@ module.exports = function (app) {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static("public"));
-  console.log(config.get("cookiePrivateKey"));
   app.use(cookieParser(config.get("cookiePrivateKey")));
 
   //routes
   app.use("/", home);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use("/api/inbox", inbox);
 
   //error
   app.use(error);
